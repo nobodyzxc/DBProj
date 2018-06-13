31ff3fa687e87fd4669a4bcceabe26c57d2d43da
Simon Lee <simon2020291@gmail.com>
2018-06-13 18:23:21 +0800

    delete post function

--- views/manage.py
+++ views/manage.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, flash, url_for
 from flask_login import login_required, current_user
 from app import app, users, db_name
 from module.db import query, alter
+import re
 
 manage_pages = Blueprint('manage_pages', __name__,
                         template_folder='templates')
@@ -27,3 +28,14 @@ def manage():
     # delete function
     return render_template('manage.html', name = current_user.username,
                            title = title, postid = postid, date = date, length = length)
+
+@manage_pages.route('/manage/delete_post', methods=['GET', 'POST'])
+def delete_post():
+    if request.method == 'POST':
+        postid = re.findall('[0-9]', request.form["postid"])
+        postid = int(''.join(postid))
+        alter(db_name, """delete from post
+                        where postid = {} and owner= {}""".format(postid,
+                                                        "'"+current_user.username+"'"))
+
+    return redirect(url_for('manage_pages.manage'))
