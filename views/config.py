462d3f01f913e9af218e62710fc60f449b6e5517
Bin-Jing <jeff20001234@yahoo.com.tw>
2018-06-15 15:54:32 +0800

    config

--- views/config.py
+++ views/config.py
@@ -5,10 +5,24 @@ from module.db import query, alter
 
 config_pages = Blueprint('config_pages', __name__,
                         template_folder='templates')
+title = ["title1","title2","title3","title4"]
+allLayout = [["aaa","bbb","ccc"],["aa","bb"]]
 
 # layout and extension market here
-@config_pages.route('/config')
+@config_pages.route('/config', methods=['GET' , 'POST'])
 @login_required
 def config():
-    return render_template('config.html')
+    selectlist = []
+    
+    selectlist.append(request.form.getlist('allLayout'))
+    listToStr = ','.join(letters(str(r)) for v in selectlist for r in v)
+    print(listToStr)
+    allLayout[0] = getLayoutName(db_name,"LAYOUTNAME")
+    return render_template('config.html',allLayout=allLayout,LayoutTitle = title)
 
+def getLayoutName(db,layout):
+    return query(db, """
+            SELECT LAYOUTNAME FROM LAYOUT
+                """.format(LAYOUTNAME = layout))
+def letters(input):
+    return ''.join([str(c) for c in input if (c.isalpha())])
